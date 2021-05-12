7b6166ee2f2e2de3b9b2aa41a112f48887d79ae1
manvantar <manukvshetty@gmail.com>
2021-05-12 15:10:15 +0530

    Added: logic to find maximum diced value

---
+++ DiceProblem.js
@@ -0,0 +1,58 @@
+/*This method is used to roll a dice
+ *@return ramdom generated value between 1-6
+*/
+function rollDice() {
+    var dice = Math.floor((Math.random() * 6)) + 1;
+    return dice;
+}
+
+/*This method is used to find min and max values in an array
+ *@param takes array as in input 
+ *@return map of minimum index Array,and Maximum index Array
+*/
+function minMax(numberArray) {
+    let arrayMap = new Map();
+    let max = numberArray[1]; let min = numberArray[1];
+    let maxArray = new Array();
+    let minArray = new Array();
+    let minIndex = 1; let maxIndex = 1;
+    for (i = 2; i < numberArray.length; i++) {
+        if (numberArray[i] <= min) {
+            min = numberArray[i];
+            minIndex = i;
+        }
+        if (numberArray[i] >= max) {
+            max = numberArray[i];
+            maxIndex = i;
+        }
+    }
+    for (i = 1; i < numberArray.length; i++) {
+        if (numberArray[maxIndex] == numberArray[i]) {
+            maxArray.push(i);
+        }
+        if (numberArray[minIndex] == numberArray[i]) {
+            minArray.push(i);
+        }
+    }
+    arrayMap.set("max", maxArray);
+    arrayMap.set("min", minArray);
+    return arrayMap;
+}
+
+let diceEnries = new Map();
+for (i = 1; i <= 10; i++) {
+    diceEnries.set(i, rollDice());
+}
+
+for (let [key, value] of diceEnries.entries()) console.log("Dice" + key + ' = ' + value)
+let entryTimes = new Array(0, 0, 0, 0, 0, 0, 0);
+
+for (let value of diceEnries.values()) {
+
+    entryTimes[value] = entryTimes[value] + 1;
+}
+
+console.log(entryTimes)
+let asse = minMax(entryTimes);
+console.log("max values: " + asse.get("max") + " we got " + entryTimes[asse.get("max")[0]] + " times")
+console.log("min values: " + asse.get("min") + " we got " + entryTimes[asse.get("min")[0]] + " times")
